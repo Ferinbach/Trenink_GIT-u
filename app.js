1c6da27d973eff6aec49fc6bb27983768b31e020
František Bachratý <ferinbach@gmail.com>
2023-01-10 13:39:26 +0100

    Pridanie ďalšiej funkcie do app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+function app() {
+    if (loggedIn()) {
+        // redirect
+    }
+}
+
 console.log ("serving fish");
 console.log ("oh no");
 console.log ("bombo-lombo-colombo");
+
 function loggedIn() {
     return true;
 }
